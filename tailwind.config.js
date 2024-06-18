a6f67376fb3623c2a9898cc9f06eacb03ebeff33
Hephzibai <hephzibai10@gmail.com>
2024-06-18 12:19:14 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily:{'hero-font':'sriracha'}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
